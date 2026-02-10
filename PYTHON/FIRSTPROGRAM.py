787197ad74be5bbe3675c733c7dff16f5ef7b262
ayush2010-up <mukeahkumarpbh8@gmail.com>
2026-02-10 19:47:26 +0530

    final setting

---
+++ PYTHON/FIRSTPROGRAM.py
@@ -0,0 +1,40 @@
+# arthimatic operators 
+a=23
+b= 45
+
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a%b)# for finding remenders
+# print(a**b)#for finding square
+
+#relational operatrs 
+a=45
+b=34
+# print(a==b)#false
+# print(a!=b)#true
+
+#assinment operators
+ayu=25
+arna=33
+
+# print("remaning:",ayu-arna)
+# ayu+=56
+# print(ayu)
+# ayu-=34
+# print(ayu)
+
+#logical operators
+a=45
+b=35
+print(not False)
+print(not (a>b))
+
+vlu1=True
+vlu2=False
+print("and operator",vlu1 and vlu2)
+
+print("or operator",vlu1 or vlu2)
+print((a==b) or (a>b))
+
+
