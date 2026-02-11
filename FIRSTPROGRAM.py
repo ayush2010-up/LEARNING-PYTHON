15733cb949fbc204984200227bc742cd7a2b4e05
ayush2010-up <mukeahkumarpbh8@gmail.com>
2026-02-11 14:31:18 +0530

    LEC1 COMPLETED

--- FIRSTPROGRAM.py
+++ FIRSTPROGRAM.py
@@ -1,7 +1,7 @@
 # #practise question 1
-# # NUMBER = int(input("Enter a number: "))
-# # NUMBER2 = int(input("Enter another number: "))
-# # print(NUMBER + NUMBER2)
+# NUMBER = int(input("Enter a number: "))
+# NUMBER2 = int(input("Enter another number: "))
+# print(NUMBER + NUMBER2)
 
 # #practise question 2
 # side1 =int(input("Enter side length of square: "))
@@ -13,7 +13,7 @@
 # print("AVERAGE OF TWO NUMBERS IS:",(number1+number2)/2)
 
 # #practise question 4
-num1=int(input("Enter first number: "))
-num2=int(input("Enter second number: "))
-print(True if num1>=num2 else False) 
-print(num1>=num2)
+# num1=int(input("Enter first number: "))
+# num2=int(input("Enter second number: "))
+# print(True if num1>=num2 else False) 
+# print(num1>=num2) 
